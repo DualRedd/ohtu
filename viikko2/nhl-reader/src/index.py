cc8b0a95829281708973aded83622a3da4df25fb
Jonni Haapiainen <jonni.haapiainen@gmail.com>
2025-11-09 17:40:16 +0200

    Viikko 2: Listaa suomalaisten pelaajien tilastot

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,20 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    response = requests.get(url).json()
+
+    players = []
+    for player_dict in response:
+        player = Player(player_dict)
+        players.append(player)
+
+    print("Players from FIN:")
+    fin_players = [player for player in players if player.nationality == "FIN"]
+    for player in fin_players:
+        print(player)
+
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,10 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
+        self.nationality = dict['nationality']
+        self.team = dict['team']
+        self.assists = dict['assists']
+        self.goals = dict['goals']
     
     def __str__(self):
-        return self.name
+        return f"{self.name} team {self.team}: goals {self.goals}, assists {self.assists}" 
