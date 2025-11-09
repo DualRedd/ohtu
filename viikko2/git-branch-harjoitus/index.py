ebc2878a30be78f22247562e7e5d4eb8abc97c44
Jonni Haapiainen <jonni.haapiainen@gmail.com>
2025-11-09 21:31:54 +0200

    Viikko 2: Muuta index.py

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -7,9 +7,9 @@ from erotus import erotus
 logger("aloitetaan ohjelma") # muutos mainissa
 
 x = int(input("luku 1: "))
-y = int(input("luku 2: "))
-print(f"{summa(x, y)}")
-print(f"{erotus(x, y)}")
+y = int(input("luku 2: "))  
+print(f"{x} + {y} = {summa(x, y)}") # muutos mainissa
+print(f"{x} - {y} = {erotus(x, y)}") # muutos mainissa
 
 logger("lopetetaan")
 print("goodbye!") # lisäys bugikorjaus-branchissa
