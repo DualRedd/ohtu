30ccfdc5bca2397c4ef5906557e180bd6fb4d338
Jonni Haapiainen <jonni.haapiainen@gmail.com>
2025-11-30 19:54:03 +0200

    Extract function for converting a score number to corresponding string

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,29 +1,30 @@
 class TennisGame:
-    def __init__(self, player1_name, player2_name):
+    def __init__(self, player1_name: str, player2_name: str):
         self.player1_name = player1_name
         self.player2_name = player2_name
         self.player1_score = 0
         self.player2_score = 0
 
-    def won_point(self, player_name):
+    def won_point(self, player_name: str):
         if player_name == "player1":
             self.player1_score = self.player1_score + 1
         else:
             self.player2_score = self.player2_score + 1
+    
+    @classmethod
+    def score_to_string(cls, score: int) -> str | None:
+        score_map = {0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty"}
+        return score_map.get(score, "Unknown")
 
-    def get_score(self):
+    def get_score(self) -> str:
         score = ""
-        temp_score = 0
 
         if self.player1_score == self.player2_score:
-            if self.player1_score == 0:
-                score = "Love-All"
-            elif self.player1_score == 1:
-                score = "Fifteen-All"
-            elif self.player1_score == 2:
-                score = "Thirty-All"
-            else:
+            if self.player1_score > 2:
                 score = "Deuce"
+            else:
+                score = f"{self.score_to_string(self.player1_score)}-All"
+
         elif self.player1_score >= 4 or self.player2_score >= 4:
             player1_lead = self.player1_score - self.player2_score
 
@@ -36,20 +37,6 @@ class TennisGame:
             else:
                 score = "Win for player2"
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.player1_score
-                else:
-                    score = score + "-"
-                    temp_score = self.player2_score
-
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+            score = f"{self.score_to_string(self.player1_score)}-{self.score_to_string(self.player2_score)}"
 
         return score
