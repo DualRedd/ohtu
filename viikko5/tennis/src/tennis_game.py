fc72e881f5104423291c3d8ddb32c8bbd39262a5
Jonni Haapiainen <jonni.haapiainen@gmail.com>
2025-11-30 20:06:59 +0200

    Refactor get_score function

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -12,33 +12,26 @@ class TennisGame:
             self.player2_score = self.player2_score + 1
         else:
             raise ValueError("Unknown player name")
-    
+
     @classmethod
     def score_to_string(cls, score: int) -> str | None:
         score_map = {0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty"}
         return score_map.get(score, "Unknown")
 
     def get_score(self) -> str:
-        score = ""
+        if self.player1_score == self.player2_score and self.player1_score >= 3:
+            return "Deuce"
 
+        if self.player1_score >= 4 or self.player2_score >= 4:
+            score_diff = self.player1_score - self.player2_score
+            if abs(score_diff) == 1:
+                return "Advantage " + (self.player1_name if score_diff > 0 else self.player2_name)
+            else:
+                return "Win for " + (self.player1_name if score_diff > 0 else self.player2_name)
+
+        score = f"{self.score_to_string(self.player1_score)}-"
         if self.player1_score == self.player2_score:
-            if self.player1_score >= 3:
-                score = "Deuce"
-            else:
-                score = f"{self.score_to_string(self.player1_score)}-All"
-
-        elif self.player1_score >= 4 or self.player2_score >= 4:
-            player1_lead = self.player1_score - self.player2_score
-
-            if player1_lead == 1:
-                score = "Advantage player1"
-            elif player1_lead == -1:
-                score = "Advantage player2"
-            elif player1_lead >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
+            score += "All"
         else:
-            score = f"{self.score_to_string(self.player1_score)}-{self.score_to_string(self.player2_score)}"
-
+            score += self.score_to_string(self.player2_score)
         return score
